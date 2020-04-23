a4cc3dc8efa47c25264d73907d2e751e6c8a0818
Muhan Li <hanhanmumuqq@163.com>
2020-04-23 20:30:45 +0800

    initial

--- mains/main_mcarrier_maddpg.py
+++ mains/main_mcarrier_maddpg.py
@@ -38,8 +38,8 @@ save_map = {}
 # warm up should be less than one epoch
 ddpg_update_batch_size = 100
 ddpg_warmup_steps = 200
-ddpg_average_target_int = 100
-model_save_int = 100  # in episodes
+ddpg_average_target_int = 500
+model_save_int = 200  # in episodes
 profile_int = 50  # in episodes
 
 
@@ -53,8 +53,9 @@ class Critic(nn.Module):
         act_dim = action_dim * agent_num
 
         self.fc1 = nn.Linear(st_dim + act_dim, 1024)
-        self.fc2 = nn.Linear(1024, 300)
-        self.fc3 = nn.Linear(300, 1)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 300)
+        self.fc4 = nn.Linear(300, 1)
 
     # obs: batch_size * obs_dim
     def forward(self, all_states, action, other_actions):
@@ -64,7 +65,8 @@ class Critic(nn.Module):
         state_action = t.cat([all_states, action], dim=1)
         q = t.relu(self.fc1(state_action))
         q = t.relu(self.fc2(q))
-        q = self.fc3(q)
+        q = t.relu(self.fc3(q))
+        q = self.fc4(q)
         return q
     
 
@@ -131,7 +133,7 @@ if __name__ == "__main__":
 
             # model serialization
             if episode.get() % model_save_int == 0:
-                ddpg.save(model_dir, save_map, global_step.get() / 1000)
+                ddpg.save(model_dir, save_map, global_step.get())
                 logger.info("Saving model parameters, epoch={}, episode={}"
                             .format(epoch, episode))
 
@@ -217,7 +219,7 @@ if __name__ == "__main__":
                 ddpg.average_target_parameters()
 
             if render:
-                create_gif(frames, "{}/log/images/{}_{}".format(root_dir, epoch, episode))
+                create_gif(frames, "{}/log/images/{}_{}_{}".format(root_dir, epoch, episode, global_step.get()))
 
             local_step.reset()
             episode_finished = False
