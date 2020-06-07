2a0df29961ada8bc0b7956d2a76c09ca75b599ee
Muhan Li <hanhanmumuqq@163.com>
2020-06-07 15:18:24 +0800

    Finish api doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,7 @@ release = machin.__version__
 
 
 # -- General configuration ---------------------------------------------------
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
