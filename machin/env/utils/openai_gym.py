b3e3e3326c5fb9d967f97e5ecf0764c9f6145d00
Muhan Li <hanhanmumuqq@163.com>
2021-04-14 22:03:54 +0800

    Update test routine

--- machin/env/utils/openai_gym.py
+++ machin/env/utils/openai_gym.py
@@ -1,9 +1,4 @@
-def disable_view_window(display=None):
-    """
-    Args:
-        display: The X display name to use, by default it creates a new display.
-            you can set it to ":0" to use existing X displays.
-    """
+def disable_view_window():
     # Disable pop up windows and render in background
     # by injecting custom viewer constructor.
     from gym.envs.classic_control import rendering
@@ -11,11 +6,7 @@ def disable_view_window(display=None):
     org_constructor = rendering.Viewer.__init__
 
     def constructor(self, *args, **kwargs):
-        try:
-            org_constructor(self, *args, display=display, **kwargs)
-        except TypeError:
-            # display is included in args or kwargs
-            org_constructor(self, *args, **kwargs)
+        org_constructor(self, *args, **kwargs)
         self.window.set_visible(visible=False)
 
     rendering.Viewer.__init__ = constructor
