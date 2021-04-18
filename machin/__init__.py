e8917ccd661e4f6460d9c19844049b3d3f2a534e
Muhan Li <hanhanmumuqq@163.com>
2021-04-19 05:22:03 +0800

    Version number change

--- machin/__init__.py
+++ machin/__init__.py
@@ -1,4 +1,4 @@
 from . import env, frame, model, parallel, utils
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = ["env", "frame", "model", "parallel", "utils"]
--- machin/frame/algorithms/ppo.py
+++ machin/frame/algorithms/ppo.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 from .a2c import *
 
 
